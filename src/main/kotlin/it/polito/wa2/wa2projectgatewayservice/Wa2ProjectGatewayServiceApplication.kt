8ea091dfa0a5ab532c600b88d89e9a88ddfecfb0
Guido R <guidonguido@gmail.com>
2022-03-09 10:10:18 +0100

    Add routes for services

--- src/main/kotlin/it/polito/wa2/wa2projectgatewayservice/Wa2ProjectGatewayServiceApplication.kt
+++ src/main/kotlin/it/polito/wa2/wa2projectgatewayservice/Wa2ProjectGatewayServiceApplication.kt
@@ -20,6 +20,26 @@ class Wa2ProjectGatewayServiceApplication {
                         .filters { f -> f.rewritePath("notificationservice", "") }
                         .uri("lb://notificationservice")
             }
+            .route("orderService") {
+                    it -> it.path(true, "/orderservice/**")
+                .filters { f -> f.rewritePath("orderservice", "") }
+                .uri("lb://orderservice")
+            }
+            .route("warehouseService") {
+                    it -> it.path(true, "/warehouseservice/**")
+                .filters { f -> f.rewritePath("warehouseservice", "") }
+                .uri("lb://warehouseservice")
+            }
+            .route("walletService") {
+                    it -> it.path(true, "/walletservice/**")
+                .filters { f -> f.rewritePath("walletservice", "") }
+                .uri("lb://walletservice")
+            }
+            .route("catalogService") {
+                    it -> it.path(true, "/catalogservice/**")
+                .filters { f -> f.rewritePath("catalogservice", "") }
+                .uri("lb://catalogservice")
+            }
             .build()
     }
 }
